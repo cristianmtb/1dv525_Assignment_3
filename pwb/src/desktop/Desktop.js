712cfa034492bc376b80bcdef2b77046f9c3a03a
Cristian B <cristian.babesh@gmail.com>
2019-10-14 14:31:04 +0200

    added standard js and started working on the desktop

---
+++ pwb/src/desktop/Desktop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Desktop.css'
+export default class Desktop extends React.Component{
+    render()
+    {
+        return(
+            <div className = 'window'>
+                Henlo Fren
+            </div>
+        )
+    }
+}
